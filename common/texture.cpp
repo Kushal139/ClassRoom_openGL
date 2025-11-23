9d02b3a5515c036708e4b00b075b289f8389d7b6
Kushal139 <kushalbijarniya11@gmail.com>
2025-11-24 02:14:43 +0530

    more cleaning

--- common/texture.cpp
+++ common/texture.cpp
@@ -6,43 +6,7 @@
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 #include "texture.hpp"
-
-GLuint loadTexture(const char* filename)
-{
-    int width, height, channels;
-    unsigned char* data = stbi_load(filename, &width, &height, &channels, 0);
-
-    if (!data) {
-        printf("Failed to load texture: %s\n", filename);
-        return 0;
-    }
-
-    GLenum format = (channels == 3 ? GL_RGB : GL_RGBA);
-
-    GLuint tex;
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, format,
-                 width, height, 0, format,
-                 GL_UNSIGNED_BYTE, data);
-
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    stbi_image_free(data);
-	printf("Loaded texture: %s (width: %d, height: %d, channels: %d)\n", filename, width, height, channels);	
-    return tex;
-}
-
-
 
 GLuint loadBMP_custom(const char * imagepath){
 
--- common/texture.hpp
+++ common/texture.hpp
@@ -11,7 +11,5 @@ GLuint loadBMP_custom(const char * imagepath);
 
 // Load a .DDS file using GLFW's own loader
 GLuint loadDDS(const char * imagepath);
-GLuint loadTexture(const char* filename);
-
 
 #endif
